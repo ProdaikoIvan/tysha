459d9f497d6381af1ef266d4a29ee90f1cfca9dd
ProdaikoIvan <prodaikoivan@gmail.com>
2025-05-15 03:57:01 +0300

    fixed api

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,7 +1,11 @@
 import * as admin from 'firebase-admin';
-var serviceAccount = require("../serviceAccountKey.json");
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 const db = admin.firestore();
